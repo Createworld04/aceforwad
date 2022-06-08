3c137304fac7a14ba8ed16798b854a0da2c594c9
ACE <91398173+imacekun@users.noreply.github.com>
2022-06-08 13:33:23 +0530

    Add files via upload

---
+++ plugins/forward.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+
+from pyrogram import filters
+from pyrogram import Client as ace
+from pyrogram.types import Message
+from main import LOGGER, prefixes, AUTH_USERS
+from config import Config
+import time
+import os
+
+
+@ace.on_message(
+    filters.chat(AUTH_USERS) & filters.private &
+    filters.incoming & filters.command("ace", prefixes=prefixes)
+)
+async def forward(bot: ace , m: Message):
+    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel**")
+    t_chat = msg.forward_from_chat.id
+    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
+    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**")
+   # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id)
+    i_chat = msg1.forward_from_chat.id
+    s_msg = int(msg1.forward_from_message_id)
+    f_msg = int(msg2.forward_from_message_id)+1
+    await m.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file')
+    try:
+        for i in range(s_msg, f_msg):
+            try:
+                await bot.copy_message(
+                    chat_id= t_chat,
+                    from_chat_id= i_chat,
+                    message_id= i
+                )
+                time.sleep(2)
+            except Exception:
+                continue
+    except Exception as e:
+        await m.reply_text(str(e))
+    await m.reply_text("Done Forwarding")
